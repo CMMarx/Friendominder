e12d0bacb4a7506480e16e687c1dd155f48bbb2d
CMMarx <chrismo.marx@web.de>
2021-05-27 13:33:18 +0200

    added 2 tabs and contactlist

---
+++ app/src/main/java/com/example/pema_projekt/FragmentAdapter.java
@@ -0,0 +1,32 @@
+package com.example.pema_projekt;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import org.jetbrains.annotations.NotNull;
+
+public class FragmentAdapter extends FragmentStateAdapter {
+    public FragmentAdapter(@NonNull @NotNull FragmentManager fragmentManager, @NonNull @NotNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @org.jetbrains.annotations.NotNull
+    @Override
+    public Fragment createFragment(int position) {
+
+        switch (position){
+            case 1:
+                return new SecondFragment();
+        }
+        return new FirstFragment();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
---
+++ app/src/main/java/com/example/pema_projekt/RecyclerViewAdapter.java
@@ -0,0 +1,63 @@
+package com.example.pema_projekt;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+
+    Context mContext;
+    List<Contact> mData;
+
+    public RecyclerViewAdapter(Context mContext, List<Contact> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.item_contact, parent, false);
+        MyViewHolder vHolder = new MyViewHolder(v);
+        return vHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull RecyclerViewAdapter.MyViewHolder holder, int position) {
+        holder.tv_name.setText(mData.get(position).getName());
+        holder.tv_phone.setText(mData.get(position).getPhone());
+        holder.img.setImageResource(mData.get(position).getPhoto());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView tv_name, tv_phone;
+        private ImageView img;
+
+        public MyViewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+
+            tv_name = itemView.findViewById(R.id.name_contact);
+            tv_phone = itemView.findViewById(R.id.phone_contact);
+            img = itemView.findViewById(R.id.img_contact);
+        }
+    }
+}
